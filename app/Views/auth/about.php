1650b238032fa6ddac146d6294223e0e2312030a
ArfinaShellaMeilany <arfina.26mei2000@gmail.com>
2020-11-19 15:08:45 +0700

    Tucil Web Lanjut

---
+++ app/Views/auth/about.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Arfina | About</title>
+  </head>
+  <body>
+<!-- Awal About -->
+    <div class="container">
+        <div class="card" style="width: 18rem;">
+  <img src="/img/about.jpeg" class="card-img-top" alt="..." width= "200">
+  <div class="card-body">
+    <h5 class="card-title">BIODATA</h5>
+    <p class="card-text">Nama	: Arfina Shella Meilany<br>
+    	NPM		: 1817051005<br>
+    	Jurusan		: S1 Ilmu Komputer<br>
+    Email		: arfinashella@gmail.com </p>
+  </div>
+</div>
+</div>
+</body>
+
+   
+    <!-- Akhir About -->
---
+++ app/Views/auth/index.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>ArfinaShella | Tucil</title>
+  </head>
+
+  <body>
+
+<!-- Awal Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container">   
+  <a class="navbar-brand" href="#">Pemrograman Web Lanjut</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+   <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url('auth/index');?>">Home </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url('auth/about');?>">About </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url('auth/admin');?>">Admin</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url('auth/login');?>">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url('auth/register');?>">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url('auth/logout');?>">Logout</a>
+                    </li>
+                </ul>
+  </div>
+</div>
+</nav>
+<!-- Akhir Navbar -->
+
+<!-- Awal Jumbotron -->
+<div class="jumbotron jumbotron-fluid">
+  <div class="container text-center">
+    <img src="/img/foto.png"  width="250px"> 
+    <h1 class="display-4">Hello, Hyung</h1>
+    <p class="lead">Bagaimana sih Make Up untuk remaja?</p>
+  </div>
+</div>
+<!-- Akhir Jumbotron -->
+
+<!-- Awal About -->
+<div class="container text-center">
+    <h1>Make Up Remaja</h1>
+<div class="container text-center">
+  <div class="row">
+    <nav>
+    <div class="col-sm">
+      Makeup merupakan salah satu unsur kecantikan bagi para wanita, mulai dari wanita dewasa yang sudah cukup umur untuk menggunakan make up sampai remaja yang kulitnya masih cenderung terlalu sensitive untuk make up. Mereka berpendapat bahwa dengan menggunakan makeup, mereka jauh lebih percaya diri untuk keluar rumah. 
+      Penggunaan makeup kini bukan lagi hal baru bagi remaja. Sederet brand makeup ternama dari dalam dan luar negeri pun cukup populer di kalangan remaja. Sayangnya, remaja kerap kali keliru dalam mengaplikasikan makeup. Tak jarang polesan makeup yang digunakan justru membuat kamu terlihat lebih tua dari usia sebenarnya.  
+      Sebagai remaja kekinian seharusnya kamu bisa menjaga dan merawat kulit dengan memerhatikan tahapan makeup yang benar. 
+    </div>
+    </nav>
+    <div class="col-sm">
+      Berikut ini urutan makeup remaja yang cocok untuk kondisi kulit di usia belia. 
+      1. Gunakan base makeup yang ringan seperti BB cream atau CC cream
+      2. Set base makeup dengan loose powder
+      3. Perona pipi dengan warna lembut
+      4. Pertegas struktur wajah dengan pensil alis dan maskara
+      5. Sempurnakan tampilan dengan sentuhan lipstik nude.
+
+      Berikut ini merupakan beberapa produk Emina yang cocok untuk Make Up remaja:
+    </div>
+    <nav>
+  </div>
+</div>
+<div class="container pt-3">
+
+<div class="card-group">
+  <div class="card">
+    <img src="/img/bbcream.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">BB Cream</h5>
+      <p class="card-text">Emina Beauty Bliss BB Cream adalah one stop solution yang bertindak sebagai pelembab & antioksidan untuk membuat kulit Anda lebih cerah dan lembut. Sangat aman untuk penggunaan sehari-hari dengan formula ringan dan ukuran ramah perjalanan.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="/img/losepowder.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Lose Powder</h5>
+      <p class="card-text">Bedak tabur sehari-hari dengan oil control agent yang terasa ringan di kulit Anda. Menjadikan wajah Anda tampak segar, matte, dan halus sepanjang hari.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="/img/blushon.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Blush On</h5>
+      <p class="card-text"> Emina Cheeklit Pressed Powder hadir dalam 6 warna intens (Violet Berry, Cotton Candy, Cherry Blossoms, Sugarcane, Bittersweet, Marshmallow Lady) untuk menyesuaikan kepribadian dan suasana hati Anda. Warna merah muda untuk penampilan yang imut dan menawan, dan warna nudish untuk tampilan yang natural dan segar.</p>
+    </div>
+  </div>
+  <div class="card-group">
+  <div class="card">
+    <img src="/img/pensilalis.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Pensil Alis</h5>
+      <p class="card-text">Agen Alis dari Emina untuk Pensil alis ini sangat mudah diaplikasikan bahkan bagi para pemula dengan formula lembut untuk mendapatkan hasil yang natural. Tersedia dalam 2 warna (Hitam, Coklat) untuk penampilan keseharianmu!</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="/img/maskara.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Maskara</h5>
+      <p class="card-text">Beri bintang di mata Anda dengan Emina Star Lash Aqua Mascara, maskara tahan air dengan sedikit serat untuk memanjangkan bulu mata dan menonjolkan keindahan mata Anda.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="/img/lipstick.jpeg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Lipstick</h5>
+      <p class="card-text">Percantik bibir Anda untuk tampilan yang lebih mempesona dengan Emina Creamatte Metallic Edition. Krim bibir yang luar biasa dan tak tertahankan dengan hasil matte yang nyaman akan membuat Anda lengket. Vitamin E membuatnya lembut namun tetap lembut.</p>
+    </div>
+  </div>
+</div>
+
+
+    <script src="/js/bootstrap.bundle.min.js"></script>
+
+    
+  </body>
+</html>
